42db81a380bade8de507d37b6b0f361d42645c6e
Ajai John Chemmanam <ajaichemmanam@gmail.com>
2019-04-12 12:16:13 +0530

    Check for supported file types

--- object_detection_image.py
+++ object_detection_image.py
@@ -27,6 +27,9 @@ from utilities.findObjects import findObjects
 # Import Generate XMl utility
 from utilities.generateXML import generateXML
 
+# Supported files for Inference and Annotation
+supportedfiles = ["jpg", "jpeg", "png", "bmp"]
+
 
 def run_inference_for_single_image(image, graph):
     with graph.as_default():
@@ -114,82 +117,85 @@ with detection_graph.as_default():
             print("Annotating files... {0} of {1}".format(count, filesCount))
             _, filename = os.path.split(filepath)
             folder = os.path.dirname(filepath)
+            filesplit = filename.split(".")
+            if filesplit[len(filesplit)-1] in supportedfiles:
+                #ret,image = cap.read()
+                # Get File Properties
+                image = cv2.imread(filepath)
+                imageHeight, imageWidth, imageChannels = image.shape
+                imageSize = os.path.getsize(filepath)
 
-            #ret,image = cap.read()
-            # Get File Properties
-            image = cv2.imread(filepath)
-            imageHeight, imageWidth, imageChannels = image.shape
-            imageSize = os.path.getsize(filepath)
+                fileProps = fileProperties(
+                    filename, folder, filepath, imageWidth, imageHeight, imageChannels, imageSize)
+                # print("file", filepath, "folder", folder, "imgHeight", imageHeight, "imgWidth", imageWidth,
+                #       "imgChannel", imageChannels, "imgSize", imageSize)
 
-            fileProps = fileProperties(
-                filename, folder, filepath, imageWidth, imageHeight, imageChannels, imageSize)
-            # print("file", filepath, "folder", folder, "imgHeight", imageHeight, "imgWidth", imageWidth,
-            #       "imgChannel", imageChannels, "imgSize", imageSize)
+                # Get handles to input and output tensors
+                ops = tf.get_default_graph().get_operations()
+                all_tensor_names = {
+                    output.name for op in ops for output in op.outputs}
+                tensor_dict = {}
+                for key in [
+                    'num_detections', 'detection_boxes', 'detection_scores',
+                    'detection_classes', 'detection_masks'
+                ]:
+                    tensor_name = key + ':0'
+                    if tensor_name in all_tensor_names:
+                        tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+                            tensor_name)
+                if 'detection_masks' in tensor_dict:
+                    # The following processing is only for single image
+                    detection_boxes = tf.squeeze(
+                        tensor_dict['detection_boxes'], [0])
+                    detection_masks = tf.squeeze(
+                        tensor_dict['detection_masks'], [0])
+                    # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+                    real_num_detection = tf.cast(
+                        tensor_dict['num_detections'][0], tf.int32)
+                    detection_boxes = tf.slice(detection_boxes, [0, 0], [
+                        real_num_detection, -1])
+                    detection_masks = tf.slice(detection_masks, [0, 0, 0], [
+                        real_num_detection, -1, -1])
+                    detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+                        detection_masks, detection_boxes, image.shape[0], image.shape[1])
+                    detection_masks_reframed = tf.cast(
+                        tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+                    # Follow the convention by adding back the batch dimension
+                    tensor_dict['detection_masks'] = tf.expand_dims(
+                        detection_masks_reframed, 0)
+                image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
 
-            # Get handles to input and output tensors
-            ops = tf.get_default_graph().get_operations()
-            all_tensor_names = {
-                output.name for op in ops for output in op.outputs}
-            tensor_dict = {}
-            for key in [
-                'num_detections', 'detection_boxes', 'detection_scores',
-                'detection_classes', 'detection_masks'
-            ]:
-                tensor_name = key + ':0'
-                if tensor_name in all_tensor_names:
-                    tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
-                        tensor_name)
-            if 'detection_masks' in tensor_dict:
-                # The following processing is only for single image
-                detection_boxes = tf.squeeze(
-                    tensor_dict['detection_boxes'], [0])
-                detection_masks = tf.squeeze(
-                    tensor_dict['detection_masks'], [0])
-                # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
-                real_num_detection = tf.cast(
-                    tensor_dict['num_detections'][0], tf.int32)
-                detection_boxes = tf.slice(detection_boxes, [0, 0], [
-                                           real_num_detection, -1])
-                detection_masks = tf.slice(detection_masks, [0, 0, 0], [
-                                           real_num_detection, -1, -1])
-                detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
-                    detection_masks, detection_boxes, image.shape[0], image.shape[1])
-                detection_masks_reframed = tf.cast(
-                    tf.greater(detection_masks_reframed, 0.5), tf.uint8)
-                # Follow the convention by adding back the batch dimension
-                tensor_dict['detection_masks'] = tf.expand_dims(
-                    detection_masks_reframed, 0)
-            image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+                # Run inference
+                output_dict = sess.run(tensor_dict,
+                                       feed_dict={image_tensor: np.expand_dims(image, 0)})
 
-            # Run inference
-            output_dict = sess.run(tensor_dict,
-                                   feed_dict={image_tensor: np.expand_dims(image, 0)})
+                # all outputs are float32 numpy arrays, so convert types as appropriate
+                output_dict['num_detections'] = int(
+                    output_dict['num_detections'][0])
+                output_dict['detection_classes'] = output_dict[
+                    'detection_classes'][0].astype(np.uint8)
+                output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+                output_dict['detection_scores'] = output_dict['detection_scores'][0]
+                if 'detection_masks' in output_dict:
+                    output_dict['detection_masks'] = output_dict['detection_masks'][0]
 
-            # all outputs are float32 numpy arrays, so convert types as appropriate
-            output_dict['num_detections'] = int(
-                output_dict['num_detections'][0])
-            output_dict['detection_classes'] = output_dict[
-                'detection_classes'][0].astype(np.uint8)
-            output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
-            output_dict['detection_scores'] = output_dict['detection_scores'][0]
-            if 'detection_masks' in output_dict:
-                output_dict['detection_masks'] = output_dict['detection_masks'][0]
-
-            #output_dict = run_inference_for_single_image(image_np, detection_graph)
-            # Find Objects detected for writing to XML
-            objectList = findObjects(output_dict['detection_boxes'], output_dict['detection_classes'],
-                                     output_dict['detection_scores'], category_index)
-            # Generate XML
-            generateXML(fileProps, objectList)
-            # Draw boxes
-            vis_util.visualize_boxes_and_labels_on_image_array(
-                image,
-                output_dict['detection_boxes'],
-                output_dict['detection_classes'],
-                output_dict['detection_scores'],
-                category_index,
-                instance_masks=output_dict.get('detection_masks'),
-                use_normalized_coordinates=True,
-                line_thickness=8)
-            # Save the images with boxes
-            cv2.imwrite("dataset/testOutput/"+filename, image)
+                #output_dict = run_inference_for_single_image(image_np, detection_graph)
+                # Find Objects detected for writing to XML
+                objectList = findObjects(output_dict['detection_boxes'], output_dict['detection_classes'],
+                                         output_dict['detection_scores'], category_index)
+                # Generate XML
+                generateXML(fileProps, objectList)
+                # Draw boxes
+                vis_util.visualize_boxes_and_labels_on_image_array(
+                    image,
+                    output_dict['detection_boxes'],
+                    output_dict['detection_classes'],
+                    output_dict['detection_scores'],
+                    category_index,
+                    instance_masks=output_dict.get('detection_masks'),
+                    use_normalized_coordinates=True,
+                    line_thickness=8)
+                # Save the images with boxes
+                cv2.imwrite("dataset/testOutput/"+filename, image)
+            else:
+                print("Skipping Unsupported File: ", filename)
